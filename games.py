4530ad2e3a386cc807b6c55189ac2fd11197a303
PhillipFor <ged1@p-r-f.com>
2026-04-04 09:53:21 -0500

    4-4

--- games.py
+++ games.py
@@ -5,118 +5,191 @@ import pygame
 from Button import Button
 from Button import Bnd
 
-#import configparser
+# import configparser
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 
 pygame.init()
 
-font = pygame.font.SysFont('Arial', 24)
+#creen = pygame.display.set_mode((900,900))
+#look_1 = pygame.image.load('img/background.png').convert_alpha()
+#look_1 = pygame.transform.scale(look_1, (800, 800))
+#pygame.image.save(look_1, 'test.png')
+
+class c:
+	screen_width = 800
+	screen_height = 800
 
 
-class bs:
-    screen_width = 800
-    screen_height = 600
-    screen = 0
-    sizecell = 11
-    clock = pygame.time.Clock()
-    thegame = 0
-    customButton2 = 0
-    customButton3 = 0
+class v:
+	screen = 0
+	sizecell = 11
+	clock = pygame.time.Clock()
+	thegame = 0
+	customButton2 = 0
+	customButton3 = 0
+	canvan = 0
+
 
 # Configuration
 def main():
-    bs.screen =  pygame.display.set_mode((bs.screen_width, bs.screen_height))
-    VersionPF()
-    pygame.display.set_caption('Version ' + VersionPF.version + ' ' + VersionPF.date + '  ' + VersionPF.text)
-    #pygame.display.set_allow_screensaver(True)
-    icon = pygame.image.load("img/icon.png")
-    pygame.display.set_icon(icon)
-    canvas = pygame.Surface((bs.screen_width, bs.screen_height))
+	v.screen = pygame.display.set_mode((c.screen_width, c.screen_height))
+	VersionPF()
+	pygame.display.set_caption('Version ' + VersionPF.version + ' ' + VersionPF.date + '  ' + VersionPF.text)
+	pygame.display.set_allow_screensaver(True)
+	icon = pygame.image.load("img/icon.png")
+	pygame.display.set_icon(icon)
+	v.canvas = pygame.Surface((c.screen_width * 2, c.screen_height * 2))
 
-    bg = pygame.image.load("img/background.png")
+	bg = pygame.image.load("img/background.png")
 
+	v.screen.blit(bg, (0, 0))
+	pygame.display.flip()
 
-
-    bs.screen.blit(bg,(0, 0))
-    pygame.display.flip()
-
-    Which()
-
+	Which()
 
 
 def myFunction():
-    pygame.quit()
-    sys.exit()
+	pygame.quit()
+	sys.exit()
+
 
 def ggame():
-    bs.thegame = Bnd.objectsval
-    bs.customButton2.bnact(False)
-    bs.customButton3.bnact(False)
-    if bs.thegame == 1:
-        aa = Game(bs.screen)
-
+	v.thegame = Bnd.objectsval
+	v.customButton2.bnact(0)
+	v.customButton3.bnact(0)
+	if v.thegame == 1:
+		aa = Game(v.screen)
+		aa.display()
 
 
 def Which():
-    Button(bs.screen,bs.screen_width - 100 - 50, 30, 50, 30, 'Exit', myFunction)
-    bs.customButton2 = Button(bs.screen, 50, 30, 150, 30, 'Game 1', ggame , 1)
-    bs.customButton2.bncolor('', '#aaffaa', '#00ff00')
-    bs.customButton3 = Button(bs.screen, 250, 30, 150, 30, 'Game 2', ggame, 2)
-    bs.customButton3.bncolor('', '#aaffaa', '#00ff00')
-    Bnd.objectsval = 0
+	Button(v.screen, c.screen_width - 100 - 50, 30, 50, 30, 'Exit', myFunction)
+	v.customButton2 = Button(v.screen, 50, 30, 150, 30, 'Game 1', ggame, 1)
+	v.customButton2.bncolor('', '#aaffaa', '#00ff00')
+	v.customButton3 = Button(v.screen, 250, 30, 150, 30, 'Game 2', ggame, 2)
+	v.customButton3.bncolor('', '#aaffaa', '#00ff00')
+	Bnd.objectsval = 0
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
 
-        for a in Bnd.objects:
-            a.process()
-        if bs.thegame != 0:
-            break
+		for a in Bnd.objects:
+			a.process()
+		if v.thegame != 0:
+			break
 
-        pygame.display.flip()
-        bs.clock.tick(60)
-
-if bs.thegame == 1:
-    ggame()
-    sys.exit()
+		pygame.display.flip()
+		v.clock.tick(60)
 
 
+if v.thegame == 1:
+	ggame()
+	sys.exit()
 
 
-# Game loop.
-class Board:
-    def __init__(self, screen):
+def parseColour(colour):
+	if type(colour) == str:
+		# check to see if valid colour
+		return pygame.Color(colour)
+	else:
+		colourRGB = pygame.Color("white")
+		colourRGB.r = colour[0]
+		colourRGB.g = colour[1]
+		colourRGB.b = colour[2]
+		return colourRGB
 
-        pass
 
-    def draw(self):
-        pass
+def loadImage(fileName, useColorKey=False):
+	if os.path.isfile(fileName):
+		image = pygame.image.load(fileName)
+		image = image.convert_alpha()
+		# Return the image
+		return image
+	else:
+		raise Exception(f"Error loading image: {fileName} – Check filename and path?")
+
+
+class Background():
+	def __init__(self, screen):
+		self.screen = screen
+		self.colour = pygame.Color("black")
+		self.stagePosX = 0
+		self.stagePosY = 0
+		self.tileWidth = 0
+		self.tileHeight = 0
+
+
+	def setTiles(self, tiles):
+		if type(tiles) is str:
+			self.tiles = [[loadImage(tiles)]]
+		elif type(tiles[0]) is str:
+			self.tiles = [[loadImage(i) for i in tiles]]
+		else:
+			self.tiles = [[loadImage(i) for i in row] for row in tiles]
+		self.stagePosX = 0
+		self.stagePosY = 0
+		self.tileWidth = self.tiles[0][0].get_width()
+		self.tileHeight = self.tiles[0][0].get_height()
+		self.screen.blit(self.tiles[0][0], [0, 0])
+		##  self.surface = self.screen.copy()
+
+
+	def scroll(self, x, y):
+		self.stagePosX = x
+		self.stagePosY = y
+		col = (self.stagePosX % (self.tileWidth * len(self.tiles[0]))) // self.tileWidth
+		xOff = (0 - self.stagePosX % self.tileWidth)
+		row = (self.stagePosY % (self.tileHeight * len(self.tiles))) // self.tileHeight
+		yOff = (0 - self.stagePosY % self.tileHeight)
+
+		col2 = ((self.stagePosX + self.tileWidth) % (self.tileWidth * len(self.tiles[0]))) // self.tileWidth
+		row2 = ((self.stagePosY + self.tileHeight) % (self.tileHeight * len(self.tiles))) // self.tileHeight
+		self.screen.blit(self.tiles[row][col], [xOff, yOff])
+		self.screen.blit(self.tiles[row][col2], [xOff + self.tileWidth, yOff])
+		self.screen.blit(self.tiles[row2][col], [xOff, yOff + self.tileHeight])
+		self.screen.blit(self.tiles[row2][col2], [xOff + self.tileWidth, yOff + self.tileHeight])
+
 
 class Game:
-    def __init__(self, screen):
-        while True:
-            bs.screen.fill(20, 20, 20)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+	def __init__(self, scr):
+		self.screen = scr
+
+	def display(self):
+		self.canvas = pygame.Surface((c.screen_width * 2, c.screen_height * 2))
+
+		self.playerX = 0
+		self.playerY = 0
+		bg = Background(self.screen)
+		bg.setTiles("test.png")
 
 
-                pygame.display.flip()
-                bs.clock.tick(60)
+
+		while True:
+
+			bg.scroll(self.playerX, self.playerY)
+			self.playerX += 5
+			self.playerY += 1
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+
+			pygame.display.flip()
+			v.clock.tick(1)
 
 
 class VersionPF:
-    version = '0.01'
-    date = '1 Apr 2026'
-    text = 'Games - start over'
-    '''
-    0.00 21 Feb 2026 Games - start
-    '''
+	version = '0.01'
+	date = '4 Apr 2026'
+	text = 'Games - start over'
+	'''
+	0.00 1 Apr 2026 Games - start
+	'''
+
 
 if __name__ == '__main__':
-   main()
+	main()
